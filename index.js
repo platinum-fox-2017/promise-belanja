76d18b493addae313f7baa00fc51821d1d9ab240
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-20 17:47:19 +0700

    added promise chaining

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ class objItem {
         this.waktu = waktu;
     }
 }
+
+
 beli(uang, new objItem('Baju', 30000))
     .then(function(kembalian) {
         beli(kembalian, new objItem('Celana', 40000))
@@ -29,3 +31,19 @@ beli(uang, new objItem('Baju', 30000))
             .catch(function(err) {});
     })
     .catch(function(err) {});
+
+// Using Promise Chaining
+// beli(uang, new objItem('Baju', 30000))
+//     .then(function(kembalian) {
+//         return beli(kembalian, new objItem('Celana', 40000))
+//     })
+//     .then(function(kembalian) {
+//         return beli(kembalian, new objItem('Sepatu', 20000))
+//     })
+//     .then(function(kembalian) {
+//         return beli(kembalian, new objItem('Topi', 30000))
+//     })
+//     .then(function(kembalian) {
+//         return beli(kembalian, new objItem('Rompi', 40000))
+//     })
+//     .catch(function(err) {console.log(`Sisa uang anda: ${err}`);});
