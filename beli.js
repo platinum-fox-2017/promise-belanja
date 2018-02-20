a790d661987c0a1bcb172f32e0873a401b675f8d
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-20 14:47:57 +0700

    done,need feedback

--- beli.js
+++ beli.js
@@ -1,15 +1,18 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
-    }
-  }, obj.waktu);
+function beli(uang,obj) {
+  let myPromise = new Promise(function(resolve,reject){
+    console.log(`Saya pergi membeli ${obj.item}`)
+    setTimeout(function(){
+      let kembalian = uang - obj.harga
+      if (kembalian >= 0) {
+        console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+        resolve(kembalian)
+      }else{
+        console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+        reject(0)
+      }
+    }, obj.waktu);
+  });
+  return myPromise
 }
 
 module.exports = beli;
