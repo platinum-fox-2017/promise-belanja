99a98906c7f04f13afc54a4c6c02bb1976b37814
haripermadi <harinurpermadi@gmail.com>
2018-02-20 14:20:10 +0700

    release 0

--- beli.js
+++ beli.js
@@ -1,15 +1,17 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
+function beli(uang, obj){
+  return new Promise(function(resolve,reject){
+    console.log(`Saya pergi membeli ${obj.item}`)
+    setTimeout(function(){
     let kembalian = uang - obj.harga
     if (kembalian > 0) {
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
+      resolve(kembalian)
     }else{
       console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+      reject(0)
     }
   }, obj.waktu);
+  })
 }
 
 module.exports = beli;
---
+++ index.js
@@ -0,0 +1,49 @@
+const beli = require('./beli.js')
+
+
+let arrOfObj = [{
+  item : 'baju baru',
+  harga : 400,
+  waktu : 1000
+},{
+  item : 'celana baru',
+  harga : 500,
+  waktu : 2000
+},{
+  item : 'kue keranjang meleleh',
+  harga : 100,
+  waktu : 3000
+},{
+  item : 'ayam goreng bumbu seaweed',
+  harga : 100,
+  waktu : 5000
+},{
+  item : 'thai milk tea with black pearl',
+  harga : 100,
+  waktu : 6000
+}]
+beli(1000,arrOfObj[0]).then(kembalian =>{
+  console.log(`sisa uang adalah ${kembalian}`)
+  beli(kembalian,arrOfObj[1]).then(kembalianBeliItem1 =>{
+    console.log(`sisa uang adalah ${kembalianBeliItem1}`)
+    beli(kembalianBeliItem1,arrOfObj[2]).then(kembalianBeliItem2 =>{
+      console.log(`sisa uang adalah ${kembalianBeliItem2}`)
+      beli(kembalianBeliItem2,arrOfObj[3]).then(kembalianBeliItem3 =>{
+        console.log(`sisa uang adalah ${kembalianBeliItem3}`)
+        beli(kembalianBeliItem3,arrOfObj[4]).then(kembalianBeliItem4 =>{
+          console.log(`sisa uang adalah ${kembalianBeliItem4}`)
+        }).catch(errBeli4 =>{
+          console.log(errBeli4)
+        })
+      }).catch(failBeli3 => {
+        console.log(failBeli3)
+      })
+    }).catch(failBeli2 =>{
+      console.log(failBeli2)
+    })
+  }).catch(failBeli1 =>{
+    console.log(failBeli1)
+  })
+}).catch(failBeli =>{
+  console.log(failBeli)
+})
