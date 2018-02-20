ac98245b9eecce6e05454138ef6df3d9716c0188
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-20 14:58:34 +0700

    Release 0: Belanja

--- beli.js
+++ beli.js
@@ -1,15 +1,19 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+function beli(uang, obj) {
+  const promise = new Promise((resolve, reject) => {
+      setTimeout(function() {
+        if (uang - obj.harga > 0) {
+          console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${uang - obj.harga}`);
+          resolve(uang - obj.harga);
+        }
+        else {
+          console.log(`uang gk cukup nih buat beli ${obj.item}, uangnya kurang ${(uang - obj.harga) * -1}`);
+          reject((uang - obj.harga) * -1);
+        }
+      }, obj.waktu);
     }
-  }, obj.waktu);
+  );
+
+  return promise;
 }
 
 module.exports = beli;
